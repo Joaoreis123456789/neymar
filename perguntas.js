090bd89e69af410019489ede50c943802bac06a4
Joao pedro dos reis <142330996+Joaoreis123456789@users.noreply.github.com>
2024-11-18 11:51:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'Futebol',
+    'Quais times Neymar ja jogou?',
+    'Psg, Al Hilal, Santos, Barcelona'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Quem foi o tecnico da seleção em 2014',
+    'Felipão'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Flamengo ja foi rebaixado?',
+    'NÃO'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem ganhou o premio de bola de ouro de 2024?',
+    'Rodri'
+)
+criaCartao(
+    'Futebol',
+    'Pelé tem qnts copas do mundo?',
+    '3 copas'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Quem foi o primeiro campeão da copa do mundo?',
+    'Uruguai 1930'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Quem e o maior campẽao da libertadores no brasil?',
+    'Palmeiras, Flamengo, Santos e São paulo são os maiores vencedores com 3 titulos'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem e conhecido como Et no mundop do Futebol?',
+    'Lionel Messi'
+)
